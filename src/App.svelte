c1eaf26485f20fa7577b56cf7ddf0483a0faff20
rafael <rrrr0ffff@gmail.com>
2024-11-08 04:14:58 -0300

    refactor

--- src/App.svelte
+++ src/App.svelte
@@ -6,35 +6,39 @@
   class="h-dvh bg-[#f9f9f9] w-full p-6 flex flex-col items-center justify-center"
 >
   <div
-    class="w-full h-full flex flex-col md:grid md:grid-cols-4 md:grid-rows-5 lg:grid-cols-4 lg:w-3/5 lg:h-3/5 gap-4"
+    class="w-full h-full grid grid-rows sm:grid-cols-2 sm:grid-rows-5 md:grid-rows-3 md:grid-cols-4 lg:w-3/5 lg:h-3/5 gap-4"
   >
     <div
-      class="w-full h-full flex flex-col md:grid md:grid-rols-2 md:grid-cols-3 md:col-span-4 gap-4"
+      class="sm:col-span-2 order-last md:order-first grid gap-4 sm:grid-cols-2"
     >
-      <div class="h-full bg-violet-500 rounded-md">1</div>
-      <div
-        class="w-full h-full flex flex-col md:order-3 md:grid md:grid-cols-2 gap-4"
-      >
-        <div class="h-full bg-white rounded-md">2</div>
-        <div class="h-full bg-orange-300 rounded-md">3</div>
-      </div>
-      <div class="h-full bg-violet-200 rounded-md md:col-span-2 md:row-span-2">
-        4
-      </div>
+      <div class="bg-orange-100 h-full rounded-md sm:row-span-2">1-1</div>
+      <div class="bg-orange-300 h-full rounded-md sm:row-span-2">1-2</div>
     </div>
 
     <div
-      class="w-full h-full flex flex-col md:col-span-4 md:grid md:grid-cols-3 lg:grid-rows-3 gap-4"
+      class="w-full h-full grid grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 md:col-span-4 gap-4"
     >
-      <div class="h-full md:order-last bg-violet-500 rounded-md md:col-span-2">
-        5
+      <div class="h-full bg-violet-500 rounded-md sm:col-span-2">2-1</div>
+      <div class="h-full bg-white rounded-md">2-2</div>
+      <div class="h-full bg-orange-300 rounded-md row-span-2 sm:row-span-1">
+        2-3
       </div>
-      <div class="h-full bg-white rounded-md md:col-span-1">6</div>
     </div>
 
-    <div class="w-full h-full flex flex-col md:flex-row gap-4 md:col-span-4">
-      <div class="bg-orange-100 h-full rounded-md w-full">7</div>
-      <div class="bg-orange-300 h-full rounded-md w-full">8</div>
+    <div class="h-full bg-violet-200 rounded-md md:col-span-2 md:row-span-2">
+      3-1
+    </div>
+    <div
+      class="w-full h-full grid grid-rows-3 sm:grid-cols-3 sm:grid-rows-1 sm:col-span-2 lg:grid-rows-3 gap-4"
+    >
+      <div class="h-full md:order-last bg-violet-500 rounded-md sm:col-span-2">
+        4-1
+      </div>
+      <div
+        class="h-full bg-white rounded-md row-span-2 sm:row-span-1 sm:col-span-1"
+      >
+        4-2
+      </div>
     </div>
   </div>
 </main>
